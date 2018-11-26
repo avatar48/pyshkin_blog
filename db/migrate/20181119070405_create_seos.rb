cc0adcb7f1f222e1dd0db516c6c21af9d21da02a
denis <avatar48@yandex.ru>
2018-11-26 09:30:18 +0300

    fix dz

--- db/migrate/20181119070405_create_seos.rb
+++ db/migrate/20181119070405_create_seos.rb
@@ -2,11 +2,8 @@ class CreateSeos < ActiveRecord::Migration[5.2]
   def change
     create_table :seos do |t|
       t.string :title
-      t.string :string
       t.string :description
-      t.string :string
       t.string :keywords
-      t.string :string
       t.integer :seoable_id
       t.string  :seoable_type
       t.timestamps
