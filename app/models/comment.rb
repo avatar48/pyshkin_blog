48e2b2a58308eba1036442d983e7892ed4d1329b
denis <avatar48@yandex.ru>
2018-12-02 21:09:52 +0300

    fix dz

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :user, dependent: :delete, counter_cache: true
+  belongs_to :user, dependent: :destroy
   belongs_to :commentable, polymorphic: true
   validates :body, length: {minimum:5}
   validates :body, uniqueness: true
--- app/models/mark.rb
+++ app/models/mark.rb
@@ -1,5 +1,5 @@
 class Mark < ApplicationRecord
-  belongs_to :post
+  belongs_to :post, dependent: :destroy
   belongs_to :user
-  validates :stars, lnumericality: { less_than_or_equal_to: 5,  only_integer: true }
+  validates :stars, numericality: { less_than_or_equal_to: 5,  only_integer: true }
 end
