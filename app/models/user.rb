baea68b3997a17bcea6411e2aeb494413ca15337
denis <avatar48@yandex.ru>
2018-12-03 17:56:04 +0300

    oauth vk fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,14 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :name, length: {minimum:2, maximum:16}
   validates :name, uniqueness: true
+
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+
+    end
+  end
 end
